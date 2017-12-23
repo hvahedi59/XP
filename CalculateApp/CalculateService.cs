7d3bfc10efe1a0e89ed5fc5adc39bdc6d45611da
hvahedi59 <hvahedi@live.com>
2017-12-23 15:00:25 +0330

    First Version

---
+++ CalculateApp/CalculateService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculateApp
+{
+    public class CalculateService
+    {
+        static private List<float> memory = new List<float>();
+
+        public void Clear()
+        {
+
+        }
+
+        public float Calculate(string param)
+        {
+            System.Data.DataTable myDT = new System.Data.DataTable();
+            return (float)myDT.Compute(param, null);
+        }
+
+        public void ClearMemory()
+        {
+            memory.Clear();
+        }
+
+        /// <summary>
+        /// با محدودیت ذخیره یک عدد در حافظه
+        /// </summary>
+        /// <param name="param"></param>
+        public void SaveMemory(float param)
+        {
+            if (memory.Count == 0)
+                memory.Add(param);
+            else
+                memory[0] = param;
+        }
+
+        public float? ReadMemory(byte index)
+        {
+            if (index >= 0 && index < memory.Count)
+            {
+                return memory[index];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ CalculateApp/Default.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CalculateApp._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <asp:TextBox runat="server" ID="txtOperation"> </asp:TextBox>
+
+</asp:Content>
