3f6b5cfc976d34d9ebbffeaa52320fe4a6835cc5
Behjat <narsisx@gmail.com>
2017-12-24 12:55:38 +0330

    test

---
+++ CalculateApp/kalbasi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculateApp
+{
+    public class kalbasi
+    {
+        public int Add(string numbers)
+        {
+            char delimiterChar;
+            string[] stringSplited;
+            if (numbers.StartsWith("//"))
+            {
+                delimiterChar = char.Parse(numbers.Substring(2, 1));
+                string newString = numbers.Substring(4, numbers.Length - 4);
+                stringSplited = newString.Split(delimiterChar);
+            }
+            else
+                stringSplited = numbers.Split(',', '\n');
+
+            //if(stringSplited.Length > 2)
+            //foreach()
+            foreach(var item in stringSplited)
+            {
+                if (int.Parse(item) < 0)
+                    throw new FormatException();
+
+            }
+            int result = 0;
+            if (stringSplited.Length == 0)
+                return 0;
+            else if (stringSplited.Length == 1)
+                return int.Parse(stringSplited[0]);
+            else
+            {
+
+                foreach (var item in stringSplited)
+                {
+                    result += int.Parse(item);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ CalculateTest/kalbasiTest.cs
@@ -0,0 +1,94 @@
+﻿using CalculateApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateTest
+{
+    [TestClass]
+   public class kalbasiTest
+    {
+        [TestMethod]
+        public void AddWithEmptryString()
+        {
+            kalbasi kalbasiAdd = new kalbasi();
+            var result= kalbasiAdd.Add("");
+
+            Assert.AreEqual(result, 0);
+
+        }
+
+
+        [TestMethod]
+        public void AddWithOneNumberInString()
+        {
+            kalbasi kalbasiAdd = new kalbasi();
+            var result = kalbasiAdd.Add("1");
+
+            Assert.AreEqual(result, 1);
+
+        }
+
+        [TestMethod]
+        public void AddWithTwoNumberInString()
+        {
+            kalbasi kalbasiAdd = new kalbasi();
+            var result = kalbasiAdd.Add("1,2");
+            Assert.AreEqual(result, 3);
+        }
+
+        [TestMethod]
+        public void AddWithMoreThanTwoNumbersInString()
+        {
+            kalbasi kalbasiAdd = new kalbasi();
+            var result = kalbasiAdd.Add("1,2,123,1000");
+            Assert.AreEqual(result, 1126);
+        }
+
+
+        [TestMethod]
+        public void AddWithNewLine()
+        {
+            kalbasi kalbasiAdd = new kalbasi();
+            var result = kalbasiAdd.Add("1\n2,1000");
+            Assert.AreEqual(result, 1003);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void AddWithNewLineWithErorr()
+        {
+            kalbasi kalbasiAdd = new kalbasi();
+            var result = kalbasiAdd.Add("1,\n");
+        }
+
+        [TestMethod]
+        public void AddWithDelimiters()
+        {
+            kalbasi kalbasiAdd = new kalbasi();
+            var result = kalbasiAdd.Add("//;\n1;2");
+            Assert.AreEqual(result, 3);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException),"negative not allowed")]
+        public void AddWithNegativeNumbers()
+        {
+            kalbasi kalbasiAdd = new kalbasi();
+            var result = kalbasiAdd.Add("-1,2,-3");
+            
+        }
+
+        [TestMethod]
+        
+        public void AddWithIgnoreNumbers()
+        {
+           
+            
+        }
+    }
+}
